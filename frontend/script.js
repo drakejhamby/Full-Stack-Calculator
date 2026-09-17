1e325036b3a04ead52d2a9cde00fb24de97181c2
Drake Hamby <drakehamby1416@gmail.com>
2026-09-16 19:53:01 -0600

    first iteration of javascript implemented

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,20 @@
-// getting the variables
+// Calculations
+
+const display = document.getElementById('output');
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculate(){
+    try {
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
